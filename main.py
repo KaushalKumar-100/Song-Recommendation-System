39548354e180ec478258d07ba5bcc670cfe4235f
Kaushal kumar <kaushal221000@gmail.com>
2026-06-21 19:56:32 +0530

    Song Recommendation

---
+++ main.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import requests
+import pickle
+import json
+
+
+
+music=pickle.load(open("music.pkl", "rb"))
+similarity=pickle.load(open("Surrounding.pkl", "rb"))
+st.title("🎶Get All your favourite songs")
+
+
+
+
+music_list=[]
+for key,value in music['album_name'].items():
+    music_list.append((key,value))
+selected_music=st.selectbox("Select your genre music or album",music_list,format_func=lambda x:x[1])
+
+# url="https://www.youtube.com/search"
+# response=requests.get(url,params={"q":selected_music[0]})
+# print(response.json())
+def fetch_poster2(index):
+    try:
+        song_name = music['album_name'][index]
+        url="https://itunes.apple.com/search"
+        params={
+            'term':song_name,
+            'entity':"song",
+            "limit":1
+        }
+        response=requests.get(url,params=params)
+        data=response.json()
+        if data['resultCount']>0:
+            poster_url=data['results'][0]['artworkUrl100']
+            poster_url=poster_url.replace("100x100","600x600")
+            poster_link=poster_url
+        else:
+            poster_link=None
+    except Exception as e:
+        print("ERROR:", e)
+        poster_link=None
+    return poster_link
+
+
+
+def recommended(selected_music):
+    music_index=selected_music[0]
+    music_list_index =[x[0] for x in similarity[music_index]]
+    recommended_music_name=[]
+
+
+    for i in music_list_index:
+        recommended_music_name.append(music['album_name'][i])
+
+
+    return recommended_music_name,music_list_index
+
+
+# st.session_state.sidebar.text_input("Enter your Name:"
+
+
+name=st.sidebar.text_input("Enter your name:")
+st.session_state['name']=name
+
+
+# if st.button(load_history):
+
+if 'search_history' not in st.session_state:
+    st.session_state.search_history=[]
+current_poster=fetch_poster2(selected_music[0])
+
+if st.button("Similar Song"):
+    with st.spinner("Loading similar song"):
+
+        names,music_list_index=recommended(selected_music)
+        poster=[]
+        for fetch in music_list_index:
+            poster.append(fetch_poster2(fetch))
+
+
+
+
+    a=selected_music[1]
+    if a not in st.session_state.search_history:
+        st.session_state.search_history.append(a)
+
+
+    for row in range(3):
+        cols=st.columns(3)
+        for col in range(3):
+            index=row*3+col
+            with cols[col]:
+                st.text(names[index])
+                if poster[index]:
+                    st.image(poster[index],use_container_width=True)
+                else:
+                    st.write("no image Available")
+
+
+
+
+with st.sidebar.expander("Searched History"):
+    for song in st.session_state.search_history:
+        st.write(song)
+
+
+st.sidebar.image(current_poster,use_container_width=True)
+
